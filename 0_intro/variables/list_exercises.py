09839c221e8c6d202bc0a77a443c69d7280e8888
ShazamR <42967019+ShazamR@users.noreply.github.com>
2021-09-14 10:09:49 +0800

    update

---
+++ 0_intro/variables/list_exercises.py
@@ -0,0 +1,24 @@
+# list_exercises.py
+
+mylist = list(range(4))
+
+# 01. Sum all of the elements in the list
+
+
+def sum_list(list):
+    return sum(list)
+
+
+print(sum_list(mylist))
+# 02. Write a program that removes all duplicates from a list
+
+
+def remove_duplicate(list):
+    new_list = [x for x in ]
+
+    # 03. Write a program that finds the intersection of two list
+
+    # 04. Write a program that finds the union of two lists, omitting duplicates
+    # 05. Write a program that finds the differences of two lists (opposite of intersection)
+    # 06. Write a program that creates a list containing the frequencies
+    #[[1, 3]]
