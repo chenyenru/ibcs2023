6ff69e0b4f092ba5d5be38988b93b25455b70479
ChenYenRu <rebecca946487@gmail.com>
2022-11-05 23:08:15 +0800

    update

--- 3_networking/conchat-main/client/__init__.py
+++
@@ -1,3 +0,0 @@
-from .chat_client import run_client
-
-__all__ = ["run_client"]
--- 3_networking/conchat-main/common/user.py
+++
@@ -1,4 +0,0 @@
-class User:
-    def __init__(self, userid: str, username: str) -> None:
-        self.username = username
-        self.userid = userid
